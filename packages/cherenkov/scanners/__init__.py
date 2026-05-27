9d46db953366b9a4f075ec396a03f5796c3b732b
Moaid EL-Moatasem Bellah <moaidmoatasem@users.noreply.github.com>
2026-05-27 04:22:46 +0300

    feat(scanners): graduate AttackChainDetector to BaseScanner contract (#383)
    Closes #383

--- packages/cherenkov/scanners/__init__.py
+++ packages/cherenkov/scanners/__init__.py
@@ -1,5 +1,6 @@
 """cherenkov Scanners Module"""
 
+from .attack_chain_detector_scanner import AttackChainDetectorScanner
 from .header_scanner import SimpleScanner
 from .http_methods import HTTPMethodsScanner
 from .path_traversal_scanner import PathTraversalScanner
@@ -9,6 +10,7 @@ from .web.nuclei_scanner import NucleiScanner
 from .xxe_scanner import XXEScanner
 
 __all__ = [
+    "AttackChainDetectorScanner",
     "SimpleScanner",
     "SecurityHeadersScanner",
     "HTTPMethodsScanner",
